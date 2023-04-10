02291cdc09ac1c1e91d68920e7be2e8c8217a0a1
FarhanFardid <farhanfardid009@gmail.com>
2023-04-10 16:19:28 +0600

    navbar full

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import './Navbar.css';
 const Navbar = () => {
     return (
       
-        <div className='grid grid-cols-12 md:p-4 p-5 bg-slate-300 items-center justify-center'>
+        <div className='grid grid-cols-12 md:p-4 p-5 bg-slate-600 items-center justify-center'>
            
             <div className='md:col-span-3 col-span-12 items-center '>
-                <p className='text-3xl font-extrabold text-slate-950 md:ps-8 text-center p-2'>CareerFocus</p>
+                <p className='text-4xl font-extrabold text-slate-950 md:ps-8 text-center p-2'>CareerFocus</p>
             
             </div>
-            <div className='md:col-span-7 col-span-12 flex  ml-0 items-center justify-evenly font-extrabold text-gary-700 m-4 md:m-0'>
+            <div className='md:col-span-7 col-span-12 flex  ml-0 items-center justify-evenly font-bold text-white m-4 md:m-0'>
                 <Link to="/">Home</Link>
                 <Link to="/appliedJobs">Applied Jobs</Link>
                 <Link to="/statistics">Statistics</Link>
