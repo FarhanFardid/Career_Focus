b69405ff2239a5d452581ac4c564d39a638c6b9c
FarhanFardid <farhanfardid009@gmail.com>
2023-04-10 16:10:16 +0600

    navbar completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>This is Navbar</h2>
-            <div>
-                <p>CareerFocus</p>
+      
+        <div className='grid grid-cols-12 md:p-4 p-5 bg-slate-300 items-center justify-center'>
+           
+            <div className='md:col-span-3 col-span-12 items-center '>
+                <p className='text-3xl font-extrabold text-slate-950 md:ps-8 text-center p-2'>CareerFocus</p>
             
             </div>
-            <div>
+            <div className='md:col-span-7 col-span-12 flex  ml-0 items-center justify-evenly font-extrabold text-gary-700 m-4 md:m-0'>
                 <Link to="/">Home</Link>
                 <Link to="/appliedJobs">Applied Jobs</Link>
                 <Link to="/statistics">Statistics</Link>
                 <Link to="/blog">Blog</Link>
             </div>
-            <div></div>
+            <div className='col-span-12 md:col-span-2 border-2 bg-blue-600 text-white mx-auto md:px-3  md:py-3 rounded-lg font-semibold hover:bg-slate-700 p-2 m-2'><button>Start Applying</button></div>
         </div>
+      
     );
 };
 
