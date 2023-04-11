01772d62bf4cdd504e90c663a50d26530926aa8c
FarhanFardid <farhanfardid009@gmail.com>
2023-04-12 00:47:31 +0600

    Error 404

--- src/Components/Error404/Error404.jsx
+++ src/Components/Error404/Error404.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Error404 = () => {
     return (
-        <div>
-            <h2>This is error page</h2>
+        <div className='text-center p-52 bg-slate-200 '>
+            <h2 className='text-3xl font-bold text-blue-950 p-3'>Oppsss!!</h2>
+            <p className='text-4xl font-extrabold text-red-800 p-5'>Page Not Found</p>
+            <p className='text-6xl font-bold text-red-950'>Error 404</p>
         </div>
     );
 };
